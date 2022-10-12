368b40218cb33ee6c7595f008b3dc4bc7969910f
syrina gaddour <syrinagaddour11@gmail.com>
2022-10-12 10:00:56 +0100

    update home

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,10 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main className="max-w-screen-2xl mx-auto"></main>
-      <Banner />
-      {/* Product Feed */}
-
-      <ProductFeed  />
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed />
+      </main>
     </div>
   );
 }
-
